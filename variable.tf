bbe4db994e2cfa648925038bc7389a3171b71524
paul-harness <56152762+paul-harness@users.noreply.github.com>
2021-01-08 08:52:32 -0500

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,2 +1,17 @@
-variable "foo" {}
- 
+variable "subnet_id" {
+  default = "subnet-0d3650898e5d4ca9a"
+}
+
+variable "security_group_id" {
+  default = "sg-011b1f285fb3c9be1"
+}
+
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "aws_access_key" {
+}
+
+variable "aws_secret_key" {
+}
